72103de16e27244b52cb4d250a19e63c60a8e487
Vice Jukic <206936808+vice-jukic@users.noreply.github.com>
2025-04-29 22:37:32 +0200

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let wordArr = word.split("");
+    let wordReverse = "";
+    for(let i=wordArr.length-1; i>=0; i--){
+        wordReverse += wordArr[i];
+    }
 
+    return wordReverse;
 };
 
 // Do not edit below this line
