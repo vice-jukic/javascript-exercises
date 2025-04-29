86126f2fb646534551dafc45332032ca508e1604
Vice Jukic <206936808+vice-jukic@users.noreply.github.com>
2025-04-30 00:19:43 +0200

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    args.forEach(num => {
+        arr = arr.filter(item => item !== num);
+    });
+    return arr;
 };
 
 // Do not edit below this line
