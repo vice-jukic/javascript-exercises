e53f6405043c015b4c95f87a4f070537a6e01392
Vice Jukic <206936808+vice-jukic@users.noreply.github.com>
2025-04-29 21:54:44 +0200

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    if (num < 0){
+        return "ERROR"
+    }
+    let newWord = "";
+    for (let i=1; i<=num; i++){
+        newWord += word;
+    }
 
+    return newWord;
 };
 
 // Do not edit below this line
