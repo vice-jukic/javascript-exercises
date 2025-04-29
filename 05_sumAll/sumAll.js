b6285a4493ba055a7e1ac427e6434b7b293cda99
Vice Jukic <206936808+vice-jukic@users.noreply.github.com>
2025-04-30 01:36:04 +0200

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if (typeof num1 !== "number" || typeof num2 !== "number"){
+        return "ERROR";
+    }
+
+    let sum = 0;
+    let numMin = Math.min(num1, num2);
+    let numMax = Math.max(num1, num2);
+
+    let isInt = (numMin, numMax) => numMin % 1 === 0 && numMax % 1 === 0;
+
+    if (isInt(numMin, numMax) === false) {
+        return "ERROR";
+      }
+
+    if (numMin < 0 || numMax < 0){
+        return "ERROR";
+    }
+
+    for (let i=numMin; i<=numMax; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
