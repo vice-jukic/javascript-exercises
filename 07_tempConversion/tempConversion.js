f015f07096bc0f84117881fb2800cb41cc114bd6
Vice Jukic <206936808+vice-jukic@users.noreply.github.com>
2025-04-30 16:38:51 +0200

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(cel) {
+  let feh = (cel - 32) * (5/9);
+  return parseFloat(feh.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(feh) {
+  let cel = feh * (9/5) + 32;
+  return parseFloat(cel.toFixed(1));
 };
 
 // Do not edit below this line
