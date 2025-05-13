65011fe18308a0f85057d69bf0b9ca9703bfd76d
Vice Jukic <206936808+vice-jukic@users.noreply.github.com>
2025-05-13 23:23:56 +0200

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const cleanedStr = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
 
+    const reversedStr = cleanedStr.split('').reverse().join('');
+    
+    if (cleanedStr === reversedStr) {
+        return true;
+    } 
+    return false;
 };
 
 // Do not edit below this line
