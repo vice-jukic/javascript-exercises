d494f9d32632c77215a60fa62c9637e6f071178c
Vice Jukic <206936808+vice-jukic@users.noreply.github.com>
2025-05-13 23:12:01 +0200

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  for (let i=0; i<=arr.length-1; i++) {
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let prod = 1;
+  for (let i=0; i<=arr.length-1; i++) {
+    prod *= arr[i];
+  }
+  return prod;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let num = 1;
+  for (let i=2; i<=a; i++) {
+    num *= i;
+  }
+  return num
 };
 
 // Do not edit below this line
