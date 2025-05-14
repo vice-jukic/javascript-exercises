cb8dfed5f08657c90f0a26a9c6d9dde8f71c83df
Vice Jukic <206936808+vice-jukic@users.noreply.github.com>
2025-05-14 22:34:44 +0200

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    n = parseInt(num);
+
+    if (n === 1 || n === 2) {
+        return 1;
+    } else if ( n < 0 ) {
+        return "OOPS";
+    } else if (n === 0) {
+        return 0;
+    }
+
+    let n1 = 1;
+    let n2 = 1;
+    let fib = 0;
+
+    for (let i=3; i<=n; i++) {
+        fib = n1 + n2
+        n1 = n2;
+        n2 = fib;
+    }
+
+    return fib;
 };
 
 // Do not edit below this line
