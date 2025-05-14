be852abe14db14d1230f92b64e6d550bb7a679f1
Vice Jukic <206936808+vice-jukic@users.noreply.github.com>
2025-05-14 23:43:46 +0200

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    return people.reduce((oldest, person) => {
+        const deathYear = person.yearOfDeath || currentYear;
+        const age = deathYear - person.yearOfBirth;
+        
+        const oldestDeathYear = oldest.yearOfDeath || currentYear;
+        const oldestAge = oldestDeathYear - oldest.yearOfBirth;
+        
+        return age > oldestAge ? person : oldest;
+    });
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
